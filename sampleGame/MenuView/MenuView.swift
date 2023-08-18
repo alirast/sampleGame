1dc2d555e20124603eef32fc5bc1cc1f2c7aa07c
alirast <xdabnormal@gmail.com>
2023-08-18 12:12:27 +0300

    WIP: MenuView (added bestScoreLabel)

--- sampleGame/MenuView/MenuView.swift
+++ sampleGame/MenuView/MenuView.swift
@@ -10,21 +10,37 @@ import UIKit
 final class MenuView: UIView {
     
     lazy var logoView: UIImageView = {
-        var logoView = UIImageView(image: UIImage(named: "logoImage"))
+        let logoView = UIImageView(image: UIImage(named: "logoImage"))
         logoView.translatesAutoresizingMaskIntoConstraints = false
         logoView.sizeToFit()
         logoView.contentMode = .scaleAspectFit
         return logoView
     }()
     
+    lazy var bestScoreView: UILabel = {
+        let bestScoreLabel = UILabel()
+        bestScoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        bestScoreLabel.textAlignment = .center
+        bestScoreLabel.sizeToFit()
+        bestScoreLabel.numberOfLines = 1
+        bestScoreLabel.textColor = .white
+        bestScoreLabel.text = "Best Score"
+        bestScoreLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        return bestScoreLabel
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor(named: "mainGreenColor")
         addSubview(logoView)
+        addSubview(bestScoreView)
         
         NSLayoutConstraint.activate([
             logoView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
-            logoView.centerXAnchor.constraint(equalTo: centerXAnchor)
+            logoView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            bestScoreView.topAnchor.constraint(equalTo: logoView.bottomAnchor, constant: 20),
+            bestScoreView.centerXAnchor.constraint(equalTo: centerXAnchor)
         ])
     }
     
