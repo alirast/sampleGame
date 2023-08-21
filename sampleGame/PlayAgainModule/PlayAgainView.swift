e36c946a3aade709c583ad2af037e6ce6cb1e2aa
alirast <xdabnormal@gmail.com>
2023-08-21 17:55:27 +0300

    WIP: PlayAgainView (added label)

--- sampleGame/PlayAgainModule/PlayAgainView.swift
+++ sampleGame/PlayAgainModule/PlayAgainView.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class PlayAgainView: UIView {
+    
+    lazy var bestScoreLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.sizeToFit()
+        label.numberOfLines = 1
+        label.textColor = .white
+        label.text = "Best Score"
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
     lazy var playAgainButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
